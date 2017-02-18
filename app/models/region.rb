64c90256dad02b25be49aada1a12d42bc42d81f2
Lalit Adithya <lalitadithya5071@gmail.com>
2017-02-18 16:53:48 +0530

    Added district model

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,3 +1,4 @@
 class Region < ApplicationRecord
   belongs_to :zone
+  has_many :district
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218111347) do
+ActiveRecord::Schema.define(version: 20170218111715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "districts", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "region_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["region_id"], name: "index_districts_on_region_id", using: :btree
+  end
 
   create_table "regions", force: :cascade do |t|
     t.string   "name"
@@ -29,5 +37,6 @@ ActiveRecord::Schema.define(version: 20170218111347) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "districts", "regions"
   add_foreign_key "regions", "zones"
 end
