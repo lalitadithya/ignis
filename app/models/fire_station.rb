f68f770d2f1ae4f6d84147914ac466c20963a21a
Lalit Adithya <lalitadithya5071@gmail.com>
2017-03-01 16:58:25 +0530

    Fixed Pluralization in fire_station

--- app/models/fire_station.rb
+++ app/models/fire_station.rb
@@ -1,10 +1,10 @@
 class FireStation < ApplicationRecord
   belongs_to :hobli
-  has_many :vehicle
-  has_many :building
+  has_many :vehicles
+  has_many :buildings
   has_one :break_room
   has_one :telephone_room
-  has_many :office
+  has_many :offices
   has_one :class_room
-  has_many :residential_quater
+  has_many :residential_quaters
 end
